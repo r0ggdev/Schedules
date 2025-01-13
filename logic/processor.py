676e2d1f9e68c3a8536c8179d1e42a736962e5db
faryd <mirandafaryd@gmail.com>
2025-01-12 22:54:09 -0500

    ♻️ [REFACTOR] improved the readability of the code and modularized logic

--- logic/processor.py
+++ logic/processor.py
@@ -1,84 +1,82 @@
 from datetime import datetime
-from manager import DriverManager
-from scraping import Scraper
-from calendars import HorarioICSGenerator
-from cleaned import Clenaed
+from .manager import DriverManager
+from .calendars import ICSGenerator
+from .scraping import Scraper
+from .cleaned import Cleaned
+from .courses import Course
+from .user import User
 
 class Processor:
-    def __init__(self, user, password, start_date_str, end_date_str, temp_path, export_path):
-        # URL y clases
+    def __init__(self):
         self.url = 'https://intranet.upc.edu.pe/loginintranet/loginupc.aspx'
-        self.manager = DriverManager()
-        self.scraper = Scraper(self.manager, self.url)
-        self.ics_generator = HorarioICSGenerator()
-        self.cleaned = Clenaed(temp_path)
         
-        # Variables
+        self.user = ''
+        self.password = ''
+
+        self.table_info = None
+        self.table_schedule = None
+
+        self.chrome_options = ["--headless", "--disable-gpu", "--window-size=1920x1080"]
+        self.__manager = DriverManager('chrome', self.chrome_options)
+        self.scraper = Scraper(self.__manager, self.url)
+        
+        self.data_user = User()
+        self.data_courses = None
+
+    def __processCourses(data):
+        courses = {}
+
+        for course_code, course_info in data.items():
+            # Crear un objeto de la clase Course
+            course = Course(
+                code = course_info.get('codigo', None),
+                name = course_info.get('nombre', None),
+                section = course_info.get('seccion', None),
+                group = course_info.get('grupo', None),
+                lesson = course_info.get('leccion', None),
+                instructor = course_info.get('instructor', None),
+                local = course_info.get('local', None),
+                mode = course_info.get('modalidad', None)
+            )
+
+            for day, day_info in course_info['dias'].items():
+                course.addDay(day, day_info['salon'], day_info['hora_inicio'], day_info['hora_fin'])
+                courses[course_code] = course
+            
+        return courses
+
+    def setCredentials(self, user, password):
         self.user = user
         self.password = password
-        self.start_date = self.parse_date(start_date_str)
-        self.end_date = self.parse_date(end_date_str)
-        self.export_path = export_path
-        self.cleaned_schedule = self.cleaned.getCleanedScheduleName()
 
-    def parse_date(self, date_str):
-        """Convierte una cadena de fecha en formato 'ddmmyyyy' a un objeto datetime."""
-        try:
-            return datetime.strptime(date_str, "%d%m%Y")
-        except ValueError:
-            raise ValueError(f"Fecha no válida: {date_str}")
+    def scrapeData(self):
+        if self.scraper.login(self.user, self.password):
+            print("Login exitoso")
+            self.scraper.redirectSchedule()
+            
+            self.table_info, self.table_schedule = self.scraper.extractData()
 
-    def fetch_schedule_data(self):
-        """Obtiene los datos de la tabla de horarios desde el scraper."""
-        try:
-            self.scraper.login(self.user, self.password)
-
-            return self.scraper.getData() 
+            return True
         
-        except Exception as e:
-            print(f"Error al obtener los datos del horario: {e}")
-            return None, None
+    def processData(self):
+        clean_schedule = Cleaned.clean_schedule(self.table_schedule)
+        clean_user = Cleaned.cleanedInfo(self.table_info)
 
-    def save_and_clean_data(self, table_schedule, table_info):
-        """Guarda y limpia los datos obtenidos."""
-        try:
-            self.cleaned.saveTables(table_schedule, table_info)
-            self.cleaned.cleanInfo()
-        except Exception as e:
-            print(f"Error al guardar o limpiar los datos: {e}")
+        self.data_user.loadUser(clean_user)
+        self.data_courses = self.__processCourses(clean_schedule)
+    
+    def generateICS(self, path, start_date, end_date):
+        ics = ICSGenerator()
+        ics.generate(self.data_courses, path, start_date, end_date)
 
-    def generate_ics(self):
-        """Genera el archivo .ics con el horario limpio."""
-        try:
-            self.ics_generator.generar_calendario(
-                self.cleaned_schedule, self.export_path, self.start_date, self.end_date
-            )
-            print(f"Calendario generado correctamente en {self.export_path}")
-        except Exception as e:
-            print(f"Error al generar el calendario: {e}")
-
-    def process(self):
-        """Realiza todo el flujo de trabajo."""
-        table_schedule, table_info = self.fetch_schedule_data()
-        
-        if table_schedule and table_info:
-            self.save_and_clean_data(table_schedule, table_info)
-            self.generate_ics()
+    def getCodes(self):
+        return self.data_courses.keys()
+    
+    def getCourse(self, code):
+        if self.data_courses.get(code):
+            return self.data_courses[code]
         else:
-            print("No se pudo obtener la información del horario. El proceso ha fallado.")
-
-
-def main():
-    user = 'u202319239'
-    password = 'Upc_202404@'
-    start_date = "10112024"  # Fecha de inicio en formato 'ddmmyyyy'
-    end_date = "10112024"  # Fecha de fin en formato 'ddmmyyyy'
-    temp_path = './exports/temp'
-    export_path = './exports/cleaned_schedule.ics'
-
-        # Crear el procesador de horarios
-    processor = Processor(user, password, start_date, end_date, temp_path ,export_path)
-    processor.process()
-
-if __name__ == "__main__":
-    main()
+            return None
+    
+    def getDataUser(self):
+        return self.data_user
