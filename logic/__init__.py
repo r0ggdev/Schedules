b8e4a647698e33d4451d5c0daa8c8924c4148a19
faryd <mirandafaryd@gmail.com>
2025-01-12 22:55:35 -0500

    ➕ [ADD] two new classes were added

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,15 +1,3 @@
 from .processor import Processor
-
-
-'''
-user = 'a202314239'
-password = 'Upc_2024100'
-start_date = "10/11/2024"  # Fecha de inicio en formato 'ddmmyyyy'
-end_date = "10/11/2024"  # Fecha de fin en formato 'ddmmyyyy'
-temp_path = './exports/temp'
-export_path = './exports/cleaned_schedule.ics'
-
-    # Crear el procesador de horarios
-processor = Processor(user, password, start_date, end_date, temp_path ,export_path)
-processor.process()
-'''
+from .user import User
+from .courses import Course
