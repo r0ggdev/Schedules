805179c493a3e663e5b1d77151fa05be4c4e16fa
faryd <mirandafaryd@gmail.com>
2025-01-27 16:25:16 -0500

    🔧 [ADD] added gui package configuration.

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,1 +1,3 @@
-from .interface import *
+from .interface import (QMainWindow, Ui_MainWindow, QApplication, QPoint, Qt, QFileDialog, QUrl)
+from PySide6.QtGui import QIcon, QMouseEvent, QDesktopServices, QCloseEvent
+from .tools import Tools
